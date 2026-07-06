53035afa01139053eb5fc2d0234891a82872bd51
Abdullah Nazhat Abdullah <abdullah.nazhat@gmail.com>
2026-07-07 01:01:21 +0300

    Update train.py

--- train.py
+++ train.py
@@ -7,8 +7,6 @@ from torchvision import datasets
 from torchvision.transforms import ToTensor, Normalize, Compose 
 from tensormapper import TensorMapper
 
-
-
 transform = Compose([
  
 ToTensor(),
@@ -29,27 +27,20 @@ test_data = datasets.CIFAR10(
                                        download=True,
                                        transform=transform 
                                        )                                       
- 
-                                     
+                                      
 batch_size = 128
 
 train_dataloader = DataLoader(training_data, batch_size=batch_size,shuffle=True)
 test_dataloader = DataLoader(test_data, batch_size=batch_size)
-
 
 for X, y in test_dataloader:
     print(f"Shape of X [N,C,H,W]:{X.shape}")
     print(f"Shape of y:{y.shape}{y.dtype}")
     break
 
-
-
-
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
 print(f"using {device} device") 
-
-
 
 class TensorMapperImageClassification(TensorMapper):
     def __init__(
@@ -60,8 +51,7 @@ class TensorMapperImageClassification(TensorMapper):
         num_classes=10,     
         d_model = 256,
         num_layers=4,
-       
-        
+               
     ):
         super().__init__(d_model, num_layers)
         self.patcher = nn.Conv2d(
@@ -83,13 +73,8 @@ class TensorMapperImageClassification(TensorMapper):
 model = TensorMapperImageClassification().to(device)
 print(model)
 
-
-
 loss_fn = nn.CrossEntropyLoss()
 optimizer = torch.optim.Adam(model.parameters(),lr=1e-3)
-
-
-
 
 def train(dataloader, model, loss_fn, optimizer):
     size = len(dataloader.dataset)
@@ -99,21 +84,16 @@ def train(dataloader, model, loss_fn, optimizer):
     correct = 0
     for batch, (X,y) in enumerate(dataloader):
         X, y = X.to(device), y.to(device)
-       
-       
+              
         pred = model(X)
         loss = loss_fn(pred,y)
-        
-       
+               
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
         train_loss += loss.item()
         _, labels = torch.max(pred.data, 1)
         correct += labels.eq(y.data).type(torch.float).sum()
-
-        
-
 
         if batch % 100 == 0:
             loss, current = loss.item(), batch * len(X)
@@ -123,10 +103,6 @@ def train(dataloader, model, loss_fn, optimizer):
     train_accuracy = 100. * correct.item() / size
     print(train_accuracy)
     return train_loss,train_accuracy 
-
-
-
-
 
 def test(dataloader, model, loss_fn):
     size = len(dataloader.dataset)            
@@ -146,17 +122,12 @@ def test(dataloader, model, loss_fn):
     test_accuracy = 100*correct      
     return test_loss, test_accuracy
 
-
-
-
-
 logname = "/PATH/Tensor_Mapper/Experiments_cifar10/logs_tensormapper/logs_cifar10.csv"
 if not os.path.exists(logname):
   with open(logname, 'w') as logfile:
     logwriter = csv.writer(logfile, delimiter=',')
     logwriter.writerow(['epoch', 'train loss', 'train acc',
                         'test loss', 'test acc'])
-
 
 epochs = 100
 for epoch in range(epochs):
@@ -167,12 +138,10 @@ for epoch in range(epochs):
         logwriter = csv.writer(logfile, delimiter=',')
         logwriter.writerow([epoch+1, train_loss, train_acc,
                             test_loss, test_acc])
+        
 print("Done!")
-
-
 
 path = "/PATH/Tensor_Mapper/Experiments_cifar10/weights_tensormapper"
 model_name = "TensorMapperImageClassification_cifar10"
 torch.save(model.state_dict(), f"{path}/{model_name}.pth")
 print(f"Saved Model State to {path}/{model_name}.pth ")
-
